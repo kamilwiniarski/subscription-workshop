9c02f5e409fd935820c329c2f96d1ccc1ca42e74
kdrzymalski <drzymalski.krzysztof@gmail.com>
2023-03-24 13:57:24 +0100

    rewrite styles

--- src/components/SubscriptionItem.tsx
+++ src/components/SubscriptionItem.tsx
@@ -1,69 +1,11 @@
-import styled from "styled-components"
-import React, { useCallback } from "react"
-import { Subscription } from "../types/subscription"
-import { Link } from "react-router-dom"
-import { apiClient } from "../apiClient"
-import { useSubscriptionsContext } from "../contexts/subscriptionsContext/subscriptionsContext"
-import { Paragraph } from "./Paragraph"
-import { Button } from "./Button"
+import React, {useCallback} from "react"
+import {Subscription} from "../types/subscription"
+import {Link} from "react-router-dom"
+import {apiClient} from "../apiClient"
+import {useSubscriptionsContext} from "../contexts/subscriptionsContext/subscriptionsContext"
+import {Paragraph} from "./Paragraph"
+import {Button} from "./Button"
 
-const StyledSubscriptionItemContainer = styled.div`
-  background-color: rgb(18, 18, 18);
-  color: rgb(255, 255, 255);
-  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  border-radius: 4px;
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px,
-    rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
-  background-image: linear-gradient(
-    rgba(255, 255, 255, 0.05),
-    rgba(255, 255, 255, 0.05)
-  );
-  overflow: hidden;
-  max-width: 345px;
-`
-
-const StyledImage = styled.div`
-  background-image: url(https://dummyimage.com/640x360/);
-  display: block;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
-  height: 140px;
-`
-
-const CardTitle = styled.h3`
-  margin: 0 0 8px 8px;
-  font-family: Roboto, Helvetica, Arial, sans-serif;
-  font-weight: 400;
-  font-size: 1.5rem;
-  line-height: 1.334;
-  letter-spacing: 0;
-`
-
-const StyledSubscriptionContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 16px;
-  background-color: rgb(18, 18, 18);
-  color: rgb(255, 255, 255);
-  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  border-radius: 4px;
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px,
-    rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
-  background-image: linear-gradient(
-    rgba(255, 255, 255, 0.05),
-    rgba(255, 255, 255, 0.05)
-  );
-  overflow: hidden;
-  max-width: 345px;
-`
-
-const StyledActionRow = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 8px;
-`
 
 export const SubscriptionItem: React.FC<Subscription> = ({
   id,
@@ -84,16 +26,16 @@ export const SubscriptionItem: React.FC<Subscription> = ({
   }, [])
 
   return (
-    <StyledSubscriptionItemContainer>
-      <StyledImage />
-      <StyledSubscriptionContent>
-        <CardTitle>{name}</CardTitle>
+    <div className="subscription-item">
+      <div className="styled-image"></div>
+      <div className="subscription-content">
+        <h3>{name}</h3>
         <Paragraph>
           The amount of your subscription: {amount} {currency}
         </Paragraph>
         <Paragraph>Paid: {period}</Paragraph>
-      </StyledSubscriptionContent>
-      <StyledActionRow>
+      </div>
+      <div className="action-row">
         <Button>
           <Link to={`/subscription/${id}`}>Details</Link>
         </Button>
@@ -101,7 +43,7 @@ export const SubscriptionItem: React.FC<Subscription> = ({
         <Button isError={true} onClick={deleteSubscription}>
           Delete
         </Button>
-      </StyledActionRow>
-    </StyledSubscriptionItemContainer>
+      </div>
+    </div>
   )
 }
